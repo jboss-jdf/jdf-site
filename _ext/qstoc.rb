f89c9549c7b0f8e95914a971d941b57cf6ffb16e
Pete Muir <pmuir@bleepbleep.org.uk>
2012-05-29 10:45:38 +0100

    Remove empty entries

--- _ext/qstoc.rb
+++ _ext/qstoc.rb
@@ -13,7 +13,9 @@ module Awestruct
             qs_toc_content = ''
             if site.guides[@path_prefix]
               site.guides[@path_prefix].each do |guide|
-                qs_toc_content += "<tr><td align='left'><a href='#{guide.dir}' title='#{guide.dir}'>#{guide.dir}</td><td align='left'>#{split(guide.technologies)}</td><td align='left'>#{split(guide.summary)}</td><td align='left'>#{split(guide.level)}</td><td align='left'>#{split(guide.prerequisites)}</td></tr>"
+                if guide.dir
+                  qs_toc_content += "<tr><td align='left'><a href='#{site.base_url}#{guide.url}' title='#{guide.dir}'>#{guide.dir}</td><td align='left'>#{split(guide.technologies)}</td><td align='left'>#{split(guide.summary)}</td><td align='left'>#{split(guide.level)}</td><td align='left'>#{split(guide.prerequisites)}</td></tr>"
+                end
               end
               qs_toc = "<table><thead><tr><th align='left'><strong>Quickstart Name</strong></th><th align='left'><strong>Demonstrated Technologies</strong></th><th align='left'><strong>Description</strong></th><th align='left'><strong>Experience Level Required</strong></th><th align='left'><strong>Prerequisites</strong></th></tr></thead><tbody>#{qs_toc_content}</table></table>"
               page.rendered_content = page.rendered_content.gsub("\[TOC-quickstart\]", qs_toc)
