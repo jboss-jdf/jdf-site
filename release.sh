566dfd0f7a06b961c2aa1ef94baec076e7c52d96
Pete Muir <pmuir@bleepbleep.org.uk>
2012-07-06 00:10:22 +0100

    Publish before updating version no

--- release.sh
+++ release.sh
@@ -36,9 +36,9 @@ release()
    $DIR/release-utils.sh -u -o $SNAPSHOTVERSION -n $RELEASEVERSION
    git commit -a -m "Prepare for $RELEASEVERSION release"
    git tag -a $RELEASEVERSION -m "Tag $RELEASEVERSION"
+   $DIR/publish.sh -p   
    $DIR/release-utils.sh -u -o $RELEASEVERSION -n $NEWSNAPSHOTVERSION
    git commit -a -m "Prepare for development of $NEWSNAPSHOTVERSION"
-   $DIR/publish.sh -p
 }
 
 SNAPSHOTVERSION="UNDEFINED"
