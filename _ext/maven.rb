8610f8318bd22a9db228b0b9730678ab1b2bfebf
Rafael Benevides <rafabene@gmail.com>
2013-03-29 13:01:55 -0300

    Update maven extension to use _tmp

--- _ext/maven.rb
+++ _ext/maven.rb
@@ -1,18 +1,27 @@
+#awestruct issue: https://github.com/awestruct/awestruct-extensions/issues/14 asking for an official extension
 module Awestruct
   module Extensions
     class Maven
 
-      def initialize(path_prefix)
-        @path_prefix = path_prefix
+      def initialize(maven_project_path)
+        @maven_project_path = maven_project_path
       end
 
       def execute(site)
-        root_dir = site.dir.to_s.match(/^(.*[^\/?])([\/]?)$/)[1] + @path_prefix
-        pom_file = root_dir + "/pom.xml"
-        cmd = "mvn -f '#{pom_file}' clean site"
-        puts "Executing #{cmd}"
-        puts `#{cmd} `
-        puts "Maven site geneated for #{@path_prefix}"
+        maven_tmp_path =  File.join(site.tmp_dir, 'maven', @maven_project_path, 'site')
+        project_root_dir = site.dir.to_s.match(/^(.*[^\/?])([\/]?)$/)[1] + @maven_project_path
+        if !File.exist? maven_tmp_path
+          pom_file = project_root_dir + "/pom.xml"
+          cmd = "mvn -f '#{pom_file}' clean site -DsiteOutputDirectory=" + maven_tmp_path
+          puts "Executing #{cmd}"
+          puts `#{cmd} `
+          puts "Maven site geneated for #{@path_prefix}"
+        end
+        dest = project_root_dir + "/target"
+        FileUtils.remove_dir dest #clean any dirty content
+        FileUtils.mkpath dest #Create dest path if it doesn't existes
+        puts "Getting previous generated maven site from #{maven_tmp_path} to #{dest}"
+        FileUtils.cp_r maven_tmp_path, dest #copy awestruct content to project
       end
     end #End Class
   end #End module Extensions
